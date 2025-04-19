f487893b3bbcda6394c28b6a7c936dfcf51eb74e
Mercedesz Pigniczki <mercedesz.pigniczki@transferwise.com>
2025-04-19 11:48:34 +0200

    Added menu and pages

---
+++ book-club/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div style={{ textAlign: 'center', padding: '2rem' }}>
+            <h1>Welcome to the Book Club</h1>
+        </div>
+    );
+};
+
+export default Home;
